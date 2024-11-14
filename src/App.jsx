389cccf463874470160e3d75517e34f7ed36e026
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-11-14 09:14:49 +0600

    divided into pages  and create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import AddJobPage from "./pages/AddJobPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <MainLayout />,
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "/jobs",
+				element: <JobsPage />,
+			},
+			{
+				path: "/add-job",
+				element: <AddJobPage />,
+			},
+		],
+	},
+]);
 
 const App = () => {
-	return (
-		<>
-    <Navbar/>
-    <Hero/>
-    <HomeCards/>
-    <JobListings/>
-    <ViewAllJobs />
-		</>
-	);
+	return <RouterProvider router={router} />;
 };
 export default App;
